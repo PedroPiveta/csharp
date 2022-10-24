a88724762620f27936b0b38856547b27613b059e
PedroPiveta <ppievtabarrotti208@gmail.com>
2022-10-23 21:20:45 -0300

    aula40

---
+++ Aulas/aula40/aula40.cs
@@ -0,0 +1,14 @@
+using System;
+
+sealed class Veiculo{ //Não pode ser herdada!!!
+
+}
+
+class Carro:Veiculo{
+
+}
+class Aula40{
+    static void Main(){
+        Carro carro1 = new Carro();
+    }
+}
